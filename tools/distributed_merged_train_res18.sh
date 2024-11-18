9785df6d4a1447eb45e21597008c158ba595c2a0
Yang Li <1284632889@qq.com>
2024-11-18 16:11:07 +0100

    updated scripts

--- tools/distributed_merged_train_res18.sh
+++ tools/distributed_merged_train_res18.sh
@@ -3,11 +3,11 @@ set -x
 
 timestamp=`date +"%y%m%d.%H%M%S"`
 
-WORK_DIR=work_dirs/1110_map_with_other_cars
-CONFIG=projects/configs/merged_subset_A.py
+WORK_DIR=work_dirs/1114_one_stage_res18
+CONFIG=projects/configs/merged_subset_A_R18.py
 
 GPUS=$1
-PORT=${PORT:-28520}
+PORT=${PORT:-28560}
 
 python -m torch.distributed.run --nproc_per_node=$GPUS --master_port=$PORT \
     tools/merged_train.py $CONFIG --launcher pytorch --work-dir ${WORK_DIR} --deterministic ${@:2} \
